43f81e2a136d6c8e988f3c5fa9a1f4efe360e1a9
vijay <mevijayasankar@gmail.com>
2022-12-21 11:33:12 +0530

    data_type.java

---
+++ data_types.java
@@ -0,0 +1,58 @@
+package datatypes;
+
+public class data_types {
+	public static void main(String[] args) {
+		byte a=30;
+		System.out.println(a);
+		
+		short b=4040;
+	    System.out.println(b);
+	    
+	    int c=214748364;
+	    System.out.println(c);
+	    
+	    long d=21474836474l;
+	    System.out.println(d);
+	    
+	    float e=10.10f;
+	    System.out.println(e);
+	    
+	    double f=1.15d;
+	    System.out.println(f);
+	    
+	    boolean g=true;
+	    System.out.println(g);
+	    
+	    boolean h=false;
+	    System.out.println(h);
+	    
+	    char ch='5';
+	    System.out.println(ch);
+	    System.out.println(c+ch);
+	    
+	    String s="vijay 5";
+	    System.out.println(s);
+	    
+	    
+	    
+	    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
